8556d9ef8b477aceb93433894fc0152bbba3c5c2
shahrukh hussain <hshahrukh256@gmail.com>
2020-10-26 18:10:26 +0530

    passionate
    Portfolio

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $('.slider-wrapper').slick({
+        arrows: false,
+        dots: true,
+        appendDots: '.slider-dots',
+        dotsClass: 'dots'
+    });
+
+    let hamburger = document.querySelector('.hamburger');
+    let times = document.querySelector('.times');
+    let mobileNav = document.querySelector('.mobile_nav');
+
+    hamburger.addEventListener('click', function () {
+        mobileNav.classList.add('open');
+    })
+
+    times.addEventListener('click', function () {
+        mobileNav.classList.remove('open');
+
+    })
+});
